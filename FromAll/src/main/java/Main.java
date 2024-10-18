0907f47ccfba2d3a587b646013d0a73d207999c0
Ulugbek Meiliev <kurama151@mail.ru>
2024-10-18 23:40:13 +0300

    add readme.md

--- FromAll/src/main/java/Main.java
+++ FromAll/src/main/java/Main.java
@@ -1,7 +1,6 @@
-package com.UK42;
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi from master");
     }
 }
